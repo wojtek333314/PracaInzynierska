b2550aa7f8e9db25d05195a49c962f06891c8655
Wojtas <wojciech.osak@o2.pl>
2016-09-12 18:16:12 +0200

    koniec wersji podstawowej

--- app/src/main/java/brotherhood/onboardcomputer/views/recognizeButton/RecognizeButton.java
+++ app/src/main/java/brotherhood/onboardcomputer/views/recognizeButton/RecognizeButton.java
@@ -3,6 +3,8 @@ package brotherhood.onboardcomputer.views.recognizeButton;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -33,6 +35,13 @@ public class RecognizeButton extends Button {
     private ArrayList<Command> commands = new ArrayList<>();
     private TextToSpeech speaker;
     private Paint lineColor;
+    private Bitmap bar = null;
+    private Bitmap barLight;
+    private boolean animationRunning = true;
+    private int countOfBars = 17;
+    private int animationPosition = 0;
+    private boolean animationToTheRight = true;
+    private int spaceBetweenBars = 0;
 
     public RecognizeButton(Context context) {
         super(context);
@@ -48,19 +57,40 @@ public class RecognizeButton extends Button {
         setClickListener();
         createSpeaker();
         initCommands();
-        setBackgroundResource(R.drawable.red_button);
+        setBackgroundColor(getResources().getColor(android.R.color.transparent));
         lineColor = new Paint();
         lineColor.setColor(Color.RED);
         lineColor.setAntiAlias(true);
         lineColor.setStrokeWidth(50);
+
+        bar = BitmapFactory.decodeResource(getResources(),
+                R.drawable.bar);
+        barLight = BitmapFactory.decodeResource(getResources(),
+                R.drawable.bar_light);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (animationRunning) {
+                    try {
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    ((Activity) getContext()).runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            invalidate();
+                        }
+                    });
+                }
+            }
+        }).start();
     }
 
     public RecognizeButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setClickListener();
-        createSpeaker();
-        initCommands();
-        setBackgroundResource(R.drawable.red_button);
+        init();
     }
 
     private void initCommands() {
@@ -76,8 +106,6 @@ public class RecognizeButton extends Button {
             public void onClick(View v) {
                 speaker.stop();
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                //     intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                //               RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
                 intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 500);
                 intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 800);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, LOCALE_LANGUAGE);
@@ -104,9 +132,7 @@ public class RecognizeButton extends Button {
             @Override
             public void onInit(int status) {
                 if (status == TextToSpeech.SUCCESS) {
-
                     int result = speaker.setLanguage(new Locale(LOCALE_LANGUAGE));
-
                     if (result == TextToSpeech.LANG_MISSING_DATA
                             || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                         Log.e("TTS", "This Language is not supported");
@@ -121,7 +147,26 @@ public class RecognizeButton extends Button {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawRect(0, 0, getWidth(), getHeight(), lineColor);
+        spaceBetweenBars = (getWidth()- (countOfBars * bar.getWidth())) / countOfBars;
+        for (int i = 0; i < countOfBars; i++) {
+            if (i == animationPosition) {
+                canvas.drawBitmap(barLight, 90+i * bar.getWidth() + spaceBetweenBars, bar.getHeight() * 3, lineColor);
+            } else
+                canvas.drawBitmap(bar,90+i * bar.getWidth() + spaceBetweenBars, bar.getHeight() * 3, lineColor);
+        }
+
+        if (animationToTheRight)
+            animationPosition++;
+        else
+            animationPosition--;
+        if (animationPosition >= countOfBars) {
+            animationToTheRight = false;
+            animationPosition -= 2;
+        }
+        if (animationPosition < 0) {
+            animationToTheRight = true;
+            animationPosition = 1;
+        }
     }
 
     public void destroy() {
@@ -129,5 +174,7 @@ public class RecognizeButton extends Button {
             speaker.stop();
             speaker.shutdown();
         }
+
+        animationRunning = false;
     }
 }
